07103a911afd0a0c36c7b1f655f4a09a4d7bd97e
mooooburg-dev <jeongmupark@gmail.com>
2020-03-21 16:13:06 +0900

    MVC 셋팅

---
+++ jex01/src/main/java/org/zerock/config/RootConfig.java
@@ -0,0 +1,8 @@
+package org.zerock.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+
+}
---
+++ jex01/src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,27 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+	
+	
+
+}
