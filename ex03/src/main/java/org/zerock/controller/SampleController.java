e06c2b675785038a7eed219e7bc2292b853048bc
mooooburg-dev <jeongmupark@gmail.com>
2020-05-08 01:42:04 +0900

    Rest 방식 셋팅

---
+++ ex03/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,21 @@
+package org.zerock.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.log4j.Log4j;
+
+@RestController
+@RequestMapping("/sample")
+@Log4j
+public class SampleController {
+	
+	@GetMapping(value = "/getText", produces = "text/plain; charset=UTF-8")
+	public String getText() {
+		log.info("MIME TYPE : " + MediaType.TEXT_PLAIN_VALUE);
+		
+		return "안녕하세요";
+	}
+}
