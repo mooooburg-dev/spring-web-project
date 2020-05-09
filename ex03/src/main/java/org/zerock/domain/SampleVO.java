9eabe261ca97e862fad5fa672351d0f1d251884d
mooooburg-dev <jeongmupark@gmail.com>
2020-05-09 14:14:57 +0900

    REST

---
+++ ex03/src/main/java/org/zerock/domain/SampleVO.java
@@ -0,0 +1,15 @@
+package org.zerock.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SampleVO {
+
+	private Integer mno;
+	private String firstName;
+	private String lastName;
+}
