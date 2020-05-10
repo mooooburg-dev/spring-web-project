7ca13cb30bb2f9748491c51cb3855010606d6cf2
mooooburg-dev <jeongmupark@gmail.com>
2020-05-11 00:41:54 +0900

    REST

--- ex03/src/main/java/org/zerock/controller/SampleController.java
+++ ex03/src/main/java/org/zerock/controller/SampleController.java
@@ -11,9 +11,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.SampleVO;
+import org.zerock.domain.Ticket;
 
 import lombok.extern.log4j.Log4j;
 
@@ -70,5 +73,12 @@ public class SampleController {
 	@GetMapping("/product/{cat}/{pid}")
 	public String[] getPath(@PathVariable("cat") String cat, @PathVariable("pid") Integer pid){
 		return new String[] {"category:"+cat, "productid:" + pid};
+	}
+	
+	@PostMapping("/ticket")
+	public Ticket convert(@RequestBody Ticket ticket) {
+		log.info("convert............ticket" + ticket);
+		
+		return ticket;
 	}
 }
