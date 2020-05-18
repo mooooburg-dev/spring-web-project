bbb1b4e43118337d982b27d3caba50e5aee9c3db
mooooburg-dev <jeongmupark@gmail.com>
2020-05-18 11:26:50 +0900

    회사 맥에 셋팅

--- ex03/src/main/java/org/zerock/controller/BoardController.java
+++ ex03/src/main/java/org/zerock/controller/BoardController.java
@@ -39,9 +39,10 @@ public class BoardController {
 		
 		model.addAttribute("list", service.getList(cri));
 //		model.addAttribute("pageMaker", new PageDTO(cri, 123));
-		
+	
 		int total = service.getTotal(cri);
 		log.info("total : " + total);
+
 		model.addAttribute("pageMaker", new PageDTO(cri, total));
 	}
 	
