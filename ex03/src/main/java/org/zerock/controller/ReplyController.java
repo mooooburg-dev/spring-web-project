f014b0eb859764e0eae5e2df69761a03b61e3477
mooooburg-dev <jeongmupark@gmail.com>
2020-05-19 23:35:11 +0900

    에러의늪에서 벗어나다

--- ex03/src/main/java/org/zerock/controller/ReplyController.java
+++ ex03/src/main/java/org/zerock/controller/ReplyController.java
@@ -2,10 +2,13 @@ package org.zerock.controller;
 
 import java.util.List;
 
+import javax.net.ssl.HttpsURLConnection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,14 +19,12 @@ import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
-import lombok.AllArgsConstructor;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RequestMapping("/replies/")
 @RestController
 @Log4j
-@AllArgsConstructor
 public class ReplyController {
 	
 	@Setter(onMethod_ = @Autowired)
@@ -41,7 +42,7 @@ public class ReplyController {
 	}
 	
 	
-	@GetMapping(value = "/pages/{bno}/{page}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE })
+	@GetMapping(value = "/pages/{bno}/{page}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
 	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("page") int page, @PathVariable("bno") Long bno) {
 		log.info("getList.............");
 		
@@ -50,6 +51,24 @@ public class ReplyController {
 		log.info(cri);
 		
 		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
+	}
+	
+	
+	@GetMapping(value = "/{rno}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno) {
+		log.info("get : " + rno);
+		
+		return new ResponseEntity<>(service.get(rno), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{rno}", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+		log.info("remove: " + rno);
+		
+		return service.remove(rno) == 1 
+				? new ResponseEntity<>("success", HttpStatus.OK) 
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); 
+		
 	}
 	
 
