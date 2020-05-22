c34da0e9bccd78deda4c377a3eedf5331fdf8a18
mooooburg-dev <jeongmupark@gmail.com>
2020-05-22 10:41:15 +0900

    getJSON 사용하기

--- ex03/src/main/webapp/resources/js/reply.js
+++ ex03/src/main/webapp/resources/js/reply.js
@@ -22,5 +22,25 @@ var replyService = (function(){
 		})
 	}
 	
-	return {add: add};
+	function getList(param, callback, error){
+		var bno = param.bno;
+		
+		var page = param.page || 1;
+		
+		$.getJSON("/replies/pages/" + bno + "/" + page + ".json", 
+			function(data){
+				if (callback) {
+					callback(data);
+				}
+			}).fail(function(xhr, status, err){
+			if (error) {
+				error();
+			}		
+		});
+	}
+	
+	return {
+		add: add,
+		getList: getList,
+	};
 })();
